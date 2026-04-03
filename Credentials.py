88edb5d7913439d9723cffb71f11cce69c3a8853
Cavalcante <rafael.m.cavalcante@accenture.com>
2026-04-03 12:22:18 -0300

    update

--- Credentials.py
+++ Credentials.py
@@ -13,7 +13,7 @@ class MyPipefyCredentials(BaseCredentials):
 
 if __name__ == "__main__":
 
-    api_token = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3NzUwNDM0NDcsImp0aSI6ImFhZjQ0N2UyLWI1NzQtNGE3NS1hMzYxLTc3ZWM4NWYwNjA1ZiIsInN1YiI6MzA1Njc0MzA1LCJ1c2VyIjp7ImlkIjozMDU2NzQzMDUsImVtYWlsIjoicmFmYWVsLm0uY2F2YWxjYW50ZUBhY2NlbnR1cmUuY29tIn0sInVzZXJfdHlwZSI6ImF1dGhlbnRpY2F0ZWQifQ.wS973IfL2-Y3EaO4KnEt4oyz8yrLpCNjlRfoFvt2JhrcNmXSGOq_WAfgM1jAjbJCUmHpDNWbRlDJd2szDvbgmw"
+    api_token = ""
     # ============================================================
     # Setup
     # # ============================================================
